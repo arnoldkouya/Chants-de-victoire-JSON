6ed9c4a055a5b3104c84900fbb6052b944178c0f
African Hope <samuelguebo@gmail.com>
2018-05-29 11:49:46 +0000

    implement save-file module

--- parser/index.js
+++ parser/index.js
@@ -1,3 +1,15 @@
 /**
  * Main entry point of the application
  */ 
+var save = require('save-file');
+var data = "hello man";
+var root_url = "https://cantiques.yapper.fr/CV/index.html";
+
+
+save(data, '/data/test.json', (err, data) => {
+    if (err) throw err;
+ 	
+})
+
+
+
