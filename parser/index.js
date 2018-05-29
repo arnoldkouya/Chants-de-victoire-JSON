ca62ddf2cde7c7f285847732df409520710fdb94
African Hope <samuelguebo@gmail.com>
2018-05-29 15:14:03 +0000

    refactor

--- parser/index.js
+++ parser/index.js
@@ -3,39 +3,55 @@
  */
 var save = require('save-file');
 var jsdom = require("jsdom");
-var { JSDOM } = jsdom;
-var $ = require("jquery");
-
-
-var data = "hello man";
 var https = require('https');
 var root_url = "https://cantiques.yapper.fr/CV/index.html";
-var songs;
 
-urlToString(root_url).then(function(htmlContent){
-	songs = stringToDom(htmlContent).querySelector(".hymnlist li");
-	for (var i = 0; i < songs.length; i++){
-		console.log(songs[i]);
-	};
-});
-
-//songs = stringToDom(htmlContent).getElementById('top_header').innerHTML;
+/**
+ * Parse Homepage content
+ * and return list of url to crawl
+ */
+function parseHomeUrl(html){
+	let {JSDOM} = jsdom;
+  let dom = new JSDOM(html);
+  let $ = (require('jquery'))(dom.window);
+	/*
+	var parsedData = $.parseHTML(data);
+	var songs = $(data).filter("li"); */
+	//let's start extracting the data
+  var items = $("ul.hymnlist li");
+  for(var i = 0; i < items.length; i++){
+    var innerUrl = $(items[i]).children('a').attr("href");
+    console.log(innerUrl);
+  }
+}
 
 // saveData(data, './data/data.json')
 
-/**
- * Convert an HTML string string
- * into a Dom tree
- */
-function stringToDom(htmlString){
-	return htmlString.serialize().window.document;
-}
+
 /**
  * Fetching text from url
  * and returning it as a string
  */
 function urlToString(url){
-	return JSDOM.fromURL(url);
+	https.get(url, (resp) => {
+	  let data = '';
+
+	  // A chunk of data has been recieved.
+	  resp.on('data', (chunk) => {
+	    data += chunk;
+	  });
+
+	  // The whole response has been received. Print out the result.
+	  resp.on('end', () => {
+
+			return data;
+
+	  });
+
+		resp.on("error", (err) => {
+	  console.log("Error: " + err.message);
+	});
+	});
 }
 
 /**
