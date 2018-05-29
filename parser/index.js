1d6f3738ad93f21c56a1251d77b3e6bc50cd94cb
African Hope <samuelguebo@gmail.com>
2018-05-29 12:00:33 +0000

    writing to disk: successful

--- parser/index.js
+++ parser/index.js
@@ -1,26 +1,22 @@
 /**
  * Main entry point of the application
- */ 
+ */
 var save = require('save-file');
 var data = "hello man";
 var root_url = "https://cantiques.yapper.fr/CV/index.html";
 
 
-
+// Save text to disk file
 saveData(data, './data/data.json')
 
 
 /**
- * Standalone function writting text 
- * to disk
+ * Standalone function for
+ * writting text to disk
  */
 function saveData(data, path){
 	save(data, path, (err, data) => {
-    if (err) throw err;
- 	
-})
+	    if (err) throw err;
 
+	})
 }
-
-
-
